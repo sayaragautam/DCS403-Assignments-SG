ef5d0329614c679e1acde541389da9ac90357109
Sayara Gautam <sayeragtm10@gmail.com>
2026-01-19 08:05:34 +0545

    Week 2 Assignment

--- week-2/week2_day1_q1.cpp
+++ week-2/week2_day1_q1.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     int num;
-    
+
     cout << "Enter a number: ";
     cin >> num;
-    
+
     if (num > 0) {
         cout << "The number is positive";
     }
---
+++ week-3/whileloop_day1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 1;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while (i <= n) {
+        cout << i;
+
+        if (i < n) {
+            cout << "+";
+        }
+
+        i++;
+    }
+    return 0;
+}
